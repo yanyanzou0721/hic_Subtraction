b3aa3d623b84a65faf9524e7827211fcc4b8616d
yyzou <yyzou0721@outlook.com>
2019-01-09 14:14:04 +0800

    simple ussage

--- hic_Subtraction.py
+++ hic_Subtraction.py
@@ -4,7 +4,7 @@ Created on Mon Jan  7 11:29:04 2019
 
 @author: yyzou
 
-this script to get the division of two hic matrix
+this script to get the subtraction of two hic matrix
 
 input : .cool file with a certain resolution
 
@@ -31,28 +31,29 @@ def read_in(file_path):
 
 """
 
-def get_Subtraction(c1,c2,range_s,range_e):
-    mat1 = c1.matrix(balance=False, sparse=True)[range_s:range_e,range_s:range_e]
-    mat2 = c2.matrix(balance=False, sparse=True)[range_s:range_e,range_s:range_e]
+def get_Subtraction(c1,c2,area_s,area_e):
+    mat1 = c1.matrix(balance=False, sparse=True)[area_s:area_e,area_s:area_e]
+    mat2 = c2.matrix(balance=False, sparse=True)[area_s:area_e,area_s:area_e]
     arr1 = mat1.toarray()
     arr2 = mat2.toarray()
     arr_sub = arr1-arr2
-    arr_list = np.array([[x*log(x+1 if x>=0 else -x+1,10)/abs(x) for x in row ] for row in arr_sub])
+    arr_list = np.array([[x*log(x+1 if x>=0 else -x+1,10)/(abs(x)+1) for x in row ] for row in arr_sub])
     arr = arr_list.reshape(arr_list.shape[0],arr_list.shape[1])
     return arr
 
-def get_fig(data,outdir):
+
+def get_fig(data,area_s,area_e,outdir,outfig):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
-    im = ax.matshow(data, cmap='YlOrRd')
+    im = ax.matshow(data,cmap='seismic')
     fig.colorbar(im)
-    fig.savefig(outdir+"sample_div.png")
+    fig.savefig(outdir+outfig)
 
-def process(input1,input2,area_s,area_e,outdir):
+def process(input1,input2,area_s,area_e,outdir,outfig):
     c1 = read_in(input1)
     c2 = read_in(input2)
     arr = get_Subtraction(c1,c2,area_s,area_e)
-    get_fig(arr,outdir)
+    get_fig(arr,area_s,area_e,outdir,outfig)
             
     
 @click.command(name="hic_Subtraction")
@@ -67,11 +68,14 @@ def process(input1,input2,area_s,area_e,outdir):
 @click.option("--outdir", "-O",
     default="./",
     help="path to output files.")
+@click.option("--outfig", "-fig",
+    default="test.png",
+    help="name of output figure.")
 
-def main_(input1,input2,area_s,area_e,outdir):
+def main_(input1,input2,area_s,area_e,outdir,outfig):
     if not exists(outdir):
         os.mkdir(outdir)
-    process(input1,input2,area_s,area_e,outdir)
+    process(input1,input2,area_s,area_e,outdir,outfig)
     
 if __name__ == "__main__":
     main_()
