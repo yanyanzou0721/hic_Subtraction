229a365741f12b7b50dba77f62d975c756fbbba9
yyzou <yyzou0721@outlook.com>
2019-02-16 18:44:44 +0800

    normalization

--- hic_Subtraction.py
+++ hic_Subtraction.py
@@ -25,17 +25,17 @@ def read_in(file_path):
 
 """
   get matrix data then calculate input1 - input2
-
+  
   normalized by log10() with positive and negative signs unchanged
-
+  
 
 """
 
 def get_Subtraction(c1,c2,area_s,area_e):
-    mat1 = c1.matrix(balance=False, sparse=True)[area_s:area_e,area_s:area_e]
-    mat2 = c2.matrix(balance=False, sparse=True)[area_s:area_e,area_s:area_e]
-    arr1 = mat1.toarray()
-    arr2 = mat2.toarray()
+    mat1 = (c1.matrix(balance=False, sparse=True)[area_s:area_e,area_s:area_e]).toarray()
+    mat2 = (c2.matrix(balance=False, sparse=True)[area_s:area_e,area_s:area_e]).toarray()
+    arr1 = (mat1/mat1.sum())*100000
+    arr2 = (mat2/mat2.sum())*100000
     arr_sub = arr1-arr2
     arr_list = np.array([[x*log(x+1 if x>=0 else -x+1,10)/(abs(x)+1) for x in row ] for row in arr_sub])
     arr = arr_list.reshape(arr_list.shape[0],arr_list.shape[1])
@@ -45,7 +45,8 @@ def get_Subtraction(c1,c2,area_s,area_e):
 def get_fig(data,area_s,area_e,outdir,outfig):
     fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(111)
-    im = ax.matshow(data,cmap='seismic')
+    im = ax.matshow(data,cmap='seismic',vmin=-1, vmax=1,interpolation='none') ## vmin vmax determin the range of colorbar
+#    im = ax.matshow(data,cmap='YlOrRd',vmin=-1, vmax=1,interpolation='none')
     fig.colorbar(im)
     fig.savefig(outdir+outfig)
 
@@ -54,8 +55,8 @@ def process(input1,input2,area_s,area_e,outdir,outfig):
     c2 = read_in(input2)
     arr = get_Subtraction(c1,c2,area_s,area_e)
     get_fig(arr,area_s,area_e,outdir,outfig)
-
-
+            
+    
 @click.command(name="hic_Subtraction")
 @click.argument("input1")
 @click.argument("input2")
@@ -76,6 +77,6 @@ def main_(input1,input2,area_s,area_e,outdir,outfig):
     if not exists(outdir):
         os.mkdir(outdir)
     process(input1,input2,area_s,area_e,outdir,outfig)
-
+    
 if __name__ == "__main__":
     main_()
